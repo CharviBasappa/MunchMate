816e89bb92a1e0d919d5093b5a2308754bfe1317
Charvi Basappa <charvibasappa98@gmail.com>
2025-02-13 20:10:13 -0800

    Pizza-reusable component, that accepts props from its parent(App)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-const Pizza = () => {
+const Pizza = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Veggie Pizza"),
-    React.createElement("p", {}, "Mozzarella Cheese, Veggies"),
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Munch Mate"),
-    React.createElement(Pizza),
-    React.createElement(Pizza),
-    React.createElement(Pizza),
+    React.createElement(Pizza, {
+      name: "Veggie",
+      description: "Olives, Bell peppers, Jalapeno with mozarella cheese",
+    }),
+    React.createElement(Pizza, {
+      name: "Pepperoni Pizza",
+      description: "Mozarella cheese and Pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "Caprese Pizza",
+      description: "Sliced Mozzarella Cheese with diced tomatoes in pesto",
+    }),
   ]);
 };
 
