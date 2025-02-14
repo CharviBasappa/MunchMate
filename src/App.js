405ff88abb0cac2c8ddb65399b264a5783d33d71
Charvi Basappa <charvibasappa98@gmail.com>
2025-02-13 19:51:49 -0800

    A reusable component Pizza, rendered multiple times in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+const Pizza = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Veggie Pizza"),
+    React.createElement("p", {}, "Mozzarella Cheese, Veggies"),
+  ]);
+};
+
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Munch Mate")
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Munch Mate"),
+    React.createElement(Pizza),
+    React.createElement(Pizza),
+    React.createElement(Pizza),
+  ]);
 };
 
 const container = document.getElementById("root");
