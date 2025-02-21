daf3eee04a573f150dd51be0261672575461b247
Charvi Basappa <charvibasappa98@gmail.com>
2025-02-20 17:20:18 -0800

    Added images in webp and used props for image

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,21 @@ const App = () => {
   return (
     <div>
       <h1>MunchMate - Order Now!</h1>
-      <Pizza name="Pepperoni" description="Pepperoni and Cheese" />
+      <Pizza
+        name="Pepperoni"
+        description="Pepperoni and Cheese"
+        image={"/public/pizzas/pepperoni.webp"}
+      />
       <Pizza
         name="Veggies"
         description="Olives, Peppers, Tomatoes, Jalapenos"
+        image={"/public/pizzas/veggie.webp"}
       />
-      <Pizza name="Caprese" description="Sliced Mozarella with tomatoes" />
+      <Pizza
+        name="Caprese"
+        description="Sliced Mozarella with tomatoes"
+        image={"/public/pizzas/caprese.webp"}
+      />
     </div>
   );
 };
