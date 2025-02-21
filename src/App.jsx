3c8a7bfbf7b233d750658a7500cab1a0225f7ce7
Charvi Basappa <charvibasappa98@gmail.com>
2025-02-20 17:30:07 -0800

    Created order component to add pizza to cart, now it is static to test the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+// import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>MunchMate - Order Now!</h1>
-      <Pizza
-        name="Pepperoni"
-        description="Pepperoni and Cheese"
-        image={"/public/pizzas/pepperoni.webp"}
-      />
-      <Pizza
-        name="Veggies"
-        description="Olives, Peppers, Tomatoes, Jalapenos"
-        image={"/public/pizzas/veggie.webp"}
-      />
-      <Pizza
-        name="Caprese"
-        description="Sliced Mozarella with tomatoes"
-        image={"/public/pizzas/caprese.webp"}
-      />
+      <Order />
     </div>
   );
 };
