909234be393c13a13078f3830158243e33feecbf
Charvi Basappa <charvibasappa98@gmail.com>
2025-02-23 13:29:31 -0800

    Used react-query to interact with API so that it just reads the hooks and either gives loading sttaus or the data

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,6 +1,7 @@
 import { useState, useTate } from "react";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import PizzaOfTheDay from "../PizzaOfTheDay";
 import Header from "../Header";
 import { CartContext } from "../contexts";
@@ -9,13 +10,17 @@ export const Route = createRootRoute({
   component: () => {
     const cartHook = useState([]);
     return (
-      <CartContext.Provider value={cartHook}>
-        <div>
-          <Header />
-          <Outlet />
-          <PizzaOfTheDay />
-        </div>
-      </CartContext.Provider>
+      <>
+        <CartContext.Provider value={cartHook}>
+          <div>
+            <Header />
+            <Outlet />
+            <PizzaOfTheDay />
+          </div>
+        </CartContext.Provider>
+        <TanStackRouterDevtools />
+        <ReactQueryDevtools />
+      </>
     );
   },
 });
